f88a64d44b9592b13a68f6de9508822e21432460
Raylehnhoff <smdaegan@gmail.com>
2013-07-12 04:03:01 -0600

    Fixed clear issue, ship name issues

--- content/scripts/DrawShips.js
+++ content/scripts/DrawShips.js
@@ -8,7 +8,7 @@
         shipName = shipInputBox.attr("id");
         shipName = shipName.replace("ShipNumber", "");
         shipClassName = shipName;
-        if (shipName == "s300") { shipName = "300 Series"; }
+        if (shipName == "s300Series") { shipName = "300 Series"; }
         var numberToDraw = shipInputBox.val();
         if (numberToDraw > 0) {
             OutputShip(shipName, numberToDraw, shipClassName);
@@ -17,11 +17,12 @@
 }
 function OutputShip(shipName, numberToDraw, shipClassName) {
     var htmlWriter = [];
-    htmlWriter.push("<div class='ShipBox' id='" + shipClassName + "'>");
+    htmlWriter.push("<div class='ShipBox clear' id='" + shipClassName + "'>");
     htmlWriter.push("<div class='ShipName fLeft'>" + shipName + "</div>");
     for (var i = 0; i < numberToDraw; i++) {
         htmlWriter.push("<div class='Ship fLeft " + shipClassName + "'></div>");
     }
+    htmlWriter.push('<div class="clear"></div>');
     htmlWriter.push("</div>");
     $("div#ShipOutput").append(htmlWriter.join(''));
     $("div#" + shipClassName + " .ShipName").css("height", $("div#" + shipClassName).height() + "px");
