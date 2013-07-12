621830fb07de86ea212e63c3f689e474d0ea5c7f
Raylehnhoff <smdaegan@gmail.com>
2013-07-12 04:29:20 -0600

    Adding files

---
+++ content/scripts/DrawShips.js
@@ -0,0 +1,34 @@
+﻿function DrawShips() {
+    $("div#ShipOutput").html("");
+    $("dd input[type=number]").each(function () {
+        var shipInputBox = $(this);
+        var shipName;
+        var shipClassName;
+        //Validate ship names
+        shipName = shipInputBox.attr("id");
+        shipName = shipName.replace("ShipNumber", "");
+        shipClassName = shipName;
+        if (shipName == "s300Series") { shipName = "300 Series"; }
+        var numberToDraw = shipInputBox.val();
+        if (numberToDraw > 0) {
+            OutputShip(shipName, numberToDraw, shipClassName);
+        }
+    });
+
+    $("div#ShipOutput div.ShipBox").each(function () {
+        $(this).find(".ShipName").css("height", $(this).height() + "px");
+    });
+}
+function OutputShip(shipName, numberToDraw, shipClassName) {
+    var htmlWriter = [];
+    htmlWriter.push("<div class='ShipBox clear' id='" + shipClassName + "'>");
+    htmlWriter.push("<div class='ShipName fLeft'>" + shipName + "</div>");
+    htmlWriter.push("<div class='ShipContainer'>");
+    for (var i = 0; i < numberToDraw; i++) {
+        htmlWriter.push("<div class='Ship fLeft " + shipClassName + "'></div>");
+    }
+    htmlWriter.push("</div>");
+    htmlWriter.push('<div class="clear"></div>');
+    htmlWriter.push("</div>");
+    $("div#ShipOutput").append(htmlWriter.join(''));
+}
