f5a049521de5ff7786724db5b79e5de80c3e864b
Raylehnhoff <smdaegan@gmail.com>
2013-07-12 03:49:06 -0600

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ SCFSD
 =====
 
 Star Citizen Faction Ship Drawer
+
+##What is it?
+
+SCFSD is a fully-javascript tool for drawing all of the ships in your faction. Simply add the ship numbers, and it'll output for you. Simple as that.
---
+++ content/scripts/DrawShips.js
@@ -0,0 +1,28 @@
+﻿function DrawShips() {
+    $("div#ShipOutput").html("");
+    $("dd input[type=number]").each(function () {
+        var shipInputBox = $(this);
+        var shipName;
+        var shipClassName;
+        //Validate ship names
+        shipName = shipInputBox.attr("id");
+        shipName = shipName.replace("ShipNumber", "");
+        shipClassName = shipName;
+        if (shipName == "s300") { shipName = "300 Series"; }
+        var numberToDraw = shipInputBox.val();
+        if (numberToDraw > 0) {
+            OutputShip(shipName, numberToDraw, shipClassName);
+        }
+    });
+}
+function OutputShip(shipName, numberToDraw, shipClassName) {
+    var htmlWriter = [];
+    htmlWriter.push("<div class='ShipBox' id='" + shipClassName + "'>");
+    htmlWriter.push("<div class='ShipName fLeft'>" + shipName + "</div>");
+    for (var i = 0; i < numberToDraw; i++) {
+        htmlWriter.push("<div class='Ship fLeft " + shipClassName + "'></div>");
+    }
+    htmlWriter.push("</div>");
+    $("div#ShipOutput").append(htmlWriter.join(''));
+    $("div#" + shipClassName + " .ShipName").css("height", $("div#" + shipClassName).height() + "px");
+}
